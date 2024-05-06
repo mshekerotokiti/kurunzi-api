3a2c9ab60fbee27561da122cb039b42eeee99ca7
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-06 21:17:03 +0300

    :zap: controller action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,21 @@
 class ItemsController < ApplicationController
-    
+    def index
+        if params[:category_id]
+          @items = Item.where(category_id: params[:category_id])
+        else
+          @items = Item.all
+        end
+        render json: @items
+      end
+  
+
+    def show
+      item = Item.find_by(id: params[:id])
+      if item
+        render json: item, status: :ok
+      else
+        render json: { error: "Item not found" }, status: :not_found
+      end
+    end
+  
 end
