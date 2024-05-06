4159d321d778fc1d80ec9cf3a33651c1022f655a
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-06 21:09:14 +0300

    :zap: adding validations and asssociations

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,6 @@
 class BlogPost < ApplicationRecord
+    validates :image_url, presence: true
+    validates :title, presence: true
+  validates :body, presence: true
+  validates :medium_link, presence: true
 end
--- app/models/kurunzi_shop.rb
+++ app/models/kurunzi_shop.rb
@@ -1,2 +1,7 @@
 class KurunziShop < ApplicationRecord
+    belongs_to :category, optional: true
+    validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
 end
