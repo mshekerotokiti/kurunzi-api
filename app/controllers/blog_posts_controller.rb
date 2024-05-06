a97ac0e158cfa6489927dd6b1903321490557172
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-06 21:15:03 +0300

    ;zap: created the controller actions for blogs

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,7 +1,32 @@
 class BlogPostsController < ApplicationController
-  def index
-  end
+  before_action :set_blog_post, only: [:show,]
 
-  def show
+  #GET /blog_Posts
+def index
+  @blog_posts = BlogPost.all
+  render json: @blog_posts
+end
+
+# GET /blog_posts/:id
+def show
+  blog_post = BlogPost.find_by(id: params[:id])
+
+  if blog_post
+    render json: blog_post
+  else
+    render json: { error: "Blog not found" }, status: :not_found
   end
+end
+
+
+private
+
+def set_blog_post
+  @blog_post = BlogPost.find(params[:id])
+end
+
+def blog_post_params
+  params.permit(:image_url, :title, :body, :published_at)
+end
+
 end
