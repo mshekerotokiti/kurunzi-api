16651c397fdb50443b20506733d72f99ccdebeff
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-20 19:52:27 +0300

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -93,7 +93,7 @@ products = [
    name:'Lenovo Ideapad Flex 5', price:65000, description:'Intel corei5 11th Gen, 8gb RAM, 512 Gb SSD, backlit keyboard, touchscreen x360 , with Intel iris graphics', image_url:'https://laptopclinic.co.ke/cdn/shop/products/8010.jpg?v=1679501763&width=1800',category_id:laptops.id
   },
   {
-    name: 'Lenovo Thinkpad x1 yoga', price: 47000, description: ' Intel corei7 15 inches , with 8gb ram , 1tb hdd , 7200 rpm,  plus 256gb ssd , 2 gb dedicated nvidia graphics full hd , VGA  and hdmi port.', image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRX5KxnsAH-NIbbJa0JIUMecLtWpD9GldPnpirsU5N2A&s',category_id:laptops.id
+    name: 'Lenovo Thinkpad x1 yoga', price: 47000, description:' Intel corei7 15 inches , with 8gb ram , 1tb hdd , 7200 rpm,  plus 256gb ssd , 2 gb dedicated nvidia graphics full hd , VGA  and hdmi port.', image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRX5KxnsAH-NIbbJa0JIUMecLtWpD9GldPnpirsU5N2A&s',category_id:laptops.id
   },
   {
     name:'Hp 240 G8 Notebook',price:35000, description:'Intel core i5,10th Generation, 
@@ -116,13 +116,13 @@ products = [
    name:'Lenovo ThinkPad E14', price:65000, description:'Intel corei7 11th gen , 16Gb ram , 512 Gb ssd , backlit keyboard, full hd , hdmi , type c and 3 usb port ', image_url:'https://zurimall.co.ke/wp-content/uploads/2023/05/Untitled-1-2.jpg.webp', category_id:laptops.id
   },
   {
-   name:'Lenovo ThinkPad x13 ', price: 55000, description:'11th  Generation Intel Corei5,Integrated Iris Xe Graphics, 8 GB LPDDR5-6400MHz (Soldered),256gb SSD M.2 2280 ',image_url:'https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8MjY0NjY0fGltYWdlL2pwZWd8aGZhL2hlNC8xMDgxMjE3MDMzODMzNC5qcGd8MjY5MmIxNTNmYjU5ZWQ4ZjBjOGI4YTIwMjVjYzk3ZTYwMDUyNWM5N2NmOTlkNjVmZjcxNGEzNzM3ZDA5ODQ2Yw/lenovo-laptop-thinkpad-x13-gallery-2.jpg', category_id:laptops.id
+   name:'Lenovo ThinkPad x13 ', price: 55000, description:'Intel Corei5 11Gen,Integrated Iris Xe Graphics, 8 GB LPDDR5-6400MHz (Soldered),256gb SSD M.2 2280 ',image_url:'https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8MjY0NjY0fGltYWdlL2pwZWd8aGZhL2hlNC8xMDgxMjE3MDMzODMzNC5qcGd8MjY5MmIxNTNmYjU5ZWQ4ZjBjOGI4YTIwMjVjYzk3ZTYwMDUyNWM5N2NmOTlkNjVmZjcxNGEzNzM3ZDA5ODQ2Yw/lenovo-laptop-thinkpad-x13-gallery-2.jpg', category_id:laptops.id
   },
   {
-    name:' New Hp 250 g8 ', price:28000, description:'intel Celeron , 8gb  ram , 256 gb ssd, 15 inches frameless, 2 cores, Intel hd graphics,2 usb ports, type c port, hdmi port, numerical keyboard ',image_url:'https://zurimall.co.ke/wp-content/uploads/2023/03/Artboard-1-copy-2@300x-100-2023-04-01T013459.124.jpg.webp', category_id:laptops.id
+    name:' New Hp 250 g8 ', price:28000, description:'Intel Celeron , 8gb RAM , 256 gb SSD, 15 inches frameless, 2 cores, Intel hd graphics,2 usb ports, type c port, hdmi port, numerical keyboard ',image_url:'https://zurimall.co.ke/wp-content/uploads/2023/03/Artboard-1-copy-2@300x-100-2023-04-01T013459.124.jpg.webp', category_id:laptops.id
   },
   {
-    name:'Hp 250 G8 Notebook ',price:65000, description:'Intel core i7 11th Generation,  16 Gb Ram DDR4  512GB SSD (8 CPUs), 15.6 Inches size, numerical keyboard with hmdi port and type c port ',image_url:'https://support.hp.com/wcc-assets/document/images/834/c06951531.png', category_id:laptops.id
+    name:'Hp 250 G8 Notebook ',price:65000, description:'Intel core i7 11th Generation,  16 Gb RAM DDR4  512gb SSD (8 CPUs), 15.6 Inches size, numerical keyboard with hmdi port and type c port ',image_url:'https://support.hp.com/wcc-assets/document/images/834/c06951531.png', category_id:laptops.id
   },
   {
     name:'Hp 250 G7 Notebook', price: 24000, description:'Intel Corei3 7th gen processor, 15 inches , 4gb ram , 1 tb hdd 7200 rpm', image_url:'https://support.hp.com/wcc-assets/document/images/896/c06687166.png', category_id:laptops.id
@@ -131,19 +131,19 @@ products = [
     name:'Hp 250 G7 Notebook', price:45000 , description:'Intel corei5 10th gen processor, 2gb nvidia mx110 dedicated graphics, 16gb ram , 512 gb Nvme ssd, full hd ', image_url:'https://www.fgee.co.ke/wp-content/uploads/2021/07/portatil_6BP64EA_01_l.jpg', category_id:laptops.id
    },
    {
-    name:'Hp Probook 640 G1', price:20500 , description:'Intel corei5 4gb ram , 500gb hdd , 14 inches, 2.5Ghz  ', image_url:'https://www.avistel.co.ke/wp-content/uploads/2023/01/HP-ProBook-650g1-i5-4GB500GB-1.webp', category_id:laptops.id
+    name:'Hp Probook 640 G1', price:20500 , description:'Intel corei5 4gb RAM , 500gb HDD , 14 inches, 2.5Ghz  ', image_url:'https://www.avistel.co.ke/wp-content/uploads/2023/01/HP-ProBook-650g1-i5-4GB500GB-1.webp', category_id:laptops.id
    },
    {
-    name:'Hp Notebook 15', price: 27500, description:'intel corei5 7th gen processor, 8gb ram ddr4, 15 inches , numerical keyboard, 500gb hdd 7200 rpm (7200 rpm) , long lasting battery ', image_url:'https://zurimall.co.ke/wp-content/uploads/2023/08/Untitled-3678-2.jpg.webp', category_id:laptops.id
+    name:'Hp Notebook 15', price: 27500, description:'intel corei5 7th gen processor, 8gb RAM DDR4, 15 inches , numerical keyboard, 500gb HDD 7200 rpm (7200 rpm) , long lasting battery ', image_url:'https://zurimall.co.ke/wp-content/uploads/2023/08/Untitled-3678-2.jpg.webp', category_id:laptops.id
    },
    {
-    name:'Hp Probook 430 G7', price: 45000, description:'Intel corei5 16gb ram , full hd display, 256gb ssd , 10th generation processor ', image_url:'https://mtech.co.ke/media/products/2023/07/03993.jpg', category_id:laptops.id
+    name:'Hp Probook 430 G7', price: 45000, description:'Intel corei5 16gb RAM , full hd display, 256gb SSD , 10th generation processor ', image_url:'https://mtech.co.ke/media/products/2023/07/03993.jpg', category_id:laptops.id
    },
    {
-    name:'Hp Notebook 14 AMD 9 ', price:23500 , description:'AMD 9 , 4gb ram , 128 Gb ssd, with radeon 5 graphics, 14 inches ,hdmi port, 3 usb port, type c port ', image_url:'https://mtech.co.ke/media/products/2023/07/39390.jpg', category_id:laptops.id
+    name:'Hp Notebook 14 AMD 9 ', price:23500 , description:'AMD 9 , 4gb RAM, 128 Gb SSD, with radeon 5 graphics, 14 inches ,HDMI port, 3 USB port, type C port ', image_url:'https://mtech.co.ke/media/products/2023/07/39390.jpg', category_id:laptops.id
    },
    {
-    name:'Hp Probook 11 G2', price:20500 , description:'intel corei3 6th gen , touchscreen,8gb ram ,1 tb hdd, hdmi port, VGA port ', image_url:'https://www.onsidetechsolutions.co.ke/wp-content/uploads/2023/06/HP-Probook-X360-11G2-I5-10th-Gen-8GB-Ram-256GB-SSD-Touch.jpeg', category_id:laptops.id
+    name:'Hp Probook 11 G2', price:20500 , description:'Intel corei3 6th gen , touchscreen,8gb RAM ,1 tb HDD, HDMI port, VGA port ', image_url:'https://www.onsidetechsolutions.co.ke/wp-content/uploads/2023/06/HP-Probook-X360-11G2-I5-10th-Gen-8GB-Ram-256GB-SSD-Touch.jpeg', category_id:laptops.id
    },
    {
     name:'Hp Probook 445 G9 AMD RYZEN 5', price:65000 , description:'8 GB RAM DDR4, 512 GB NVME SSD, Backlit keyboard, Radeon graphics, FULL HD, 12 CPUS, 2.3 GHZ ', image_url:'https://www.saruk.co.ke/_next/image?url=https%3A%2F%2Fsaruk-images.s3.ap-south-1.amazonaws.com%2F93861706540908.jpg&w=1920&q=75',category_id:laptops.id
@@ -161,10 +161,10 @@ products = [
     name:'Hp Probook 450 G8', price:60000, description:' g8 Intel corei5 11th gen, 8gb RAM ,256gb Nvme SSD, backlit keyboard ,full hd, iris graphics',image_url:'https://zurimall.co.ke/wp-content/uploads/2023/02/Artboard-10-copy-11@300x-100-2-1-600x600-1.jpg.webp', category_id:laptops.id
    },
    {
-    name:'Hp Notebook 250 G7 ', price:45000, description:'  Intel corei5 10th gen , 2gb nvidia mx110 dedicated graphics, 16gb RAM , 512 gb Nvme SSD, full hd ',image_url:'https://www.fgee.co.ke/wp-content/uploads/2021/07/HP-Notebook-250-G7-Core-i5-4GB-RAM-1TB-Storage-15.6-FHD-Display-3.jpg', category_id:laptops.id
+    name:'Hp Notebook 250 G7 ', price:45000, description:'Intel corei5 10th gen , 2gb nvidia mx110 dedicated graphics, 16gb RAM , 512 gb Nvme SSD, full hd ',image_url:'https://www.fgee.co.ke/wp-content/uploads/2021/07/HP-Notebook-250-G7-Core-i5-4GB-RAM-1TB-Storage-15.6-FHD-Display-3.jpg', category_id:laptops.id
    },
    {
-    name:'Dell Latitude 7390', price:38500, despription:'Intel Corei5,8TH GEN,16gb RAM DDR4, 512gb NVME SSD, Touchscreen, 14inches, fingerprint scanner',image_url:'https://hewlettcomputersolution.co.ke/wp-content/uploads/2024/03/Untitled-design77-768x768.png',category_id:laptops.id
+    name:'Dell Latitude 7390', price:38500, description:'Intel Corei5,8TH GEN,16gb RAM DDR4, 512gb NVME SSD, Touchscreen, 14inches, fingerprint scanner',image_url:'https://hewlettcomputersolution.co.ke/wp-content/uploads/2024/03/Untitled-design77-768x768.png',category_id:laptops.id
    },
    {
     name:'Dell Latitude 7440', price:75000, description:'Intel Core i5-1355U 13TH Gen, 16gb RAM ,512GB SSD ,14inches, Backlit Keyboard, Intel Iris Graphics, Type C Charger',image_url:'https://www.onsidetechsolutions.co.ke/wp-content/uploads/2023/11/Dell-Latitude-7440-i7-1355U-16GB-RAM-512GB-SSD14FHD-Silver.webp',category_id:laptops.id
@@ -192,8 +192,6 @@ products = [
     name:'Hp Optical Wireless Mouse', price:500 , description:'Comfortable and durable non-slip wheel design
     light 60g button design,
     Game players level optical sensors,
-    Precise optical navigation technology
-    Ergonomic design
     High resolution up to 600-1600 DPI', image_url:'https://skywave.co.ke/wp-content/uploads/2022/08/WirelessMouse.jpg', category_id:accessories.id
    },
  
@@ -246,7 +244,7 @@ products = [
     Beat-Driven Light Show,
     PartyCast Technology,
     IPX7 Waterproof,
-    15-Hour Playtime', image_url:'https://i0.wp.com/peoplestelecom.qa/storage/2022/12/03-32.jpg?fit=800%2C800&ssl=1', category_id:accessories.id
+    15Hr Playtime', image_url:'https://i0.wp.com/peoplestelecom.qa/storage/2022/12/03-32.jpg?fit=800%2C800&ssl=1', category_id:accessories.id
    },
 
    {
@@ -255,16 +253,16 @@ products = [
 
   #  Desktops
    {
-    name:'Elitedesk 900 SSF', price: 35000, description:' Desktop PC: Intel corei7 vpro processor ,6th generation, 8gb RAM DDR(upgradable), 500 GB HDD 7200 rpm , 19 inches Tft , keyboard and mouse', image_url:'https://guavastores.com/storage/2022/04/hp-800g2-ssf.jpg', category_id:desktops.id
+    name:'Elitedesk 900 SSF', price: 35000, description:'Intel corei7 vpro processor ,6th Gen, 8gb RAM DDR(upgradable), 500 GB HDD 7200 rpm , 19 inches Tft , keyboard and mouse', image_url:'https://guavastores.com/storage/2022/04/hp-800g2-ssf.jpg', category_id:desktops.id
    },
    {
-   name:'LENOVO V530-151GB TW COMPLETE DESKTOP', description:'Intel corei5 8gen, 8GB DDR4 RAM |  1 TB (1000 GB) HDD 7200 RPM, Come with Thinkvisio screen 19.5, IPS Wide LED Screen', price:35000, image_url:'https://www.onsidetechsolutions.co.ke/wp-content/uploads/2023/06/V530-1.jpg', category_id:laptops.id
+   name:'LENOVO V530-151GB TW Complete Desktop', description:'Intel corei5 8gen, 8GB DDR4 RAM |  1 TB (1000 GB) HDD 7200 RPM, Come with Thinkvisio screen 19.5, IPS Wide LED Screen', price:35000, image_url:'https://www.onsidetechsolutions.co.ke/wp-content/uploads/2023/06/V530-1.jpg', category_id:laptops.id
    },
    {
     name:'Dell Optilex 3050', price: 15000, description:'Intel core i3 7th  gen , 8gb RAM DDR4 , 500Gb HDD 7200 rpm, with HDMI port, 3.4 GHz ', image_url:'https://www.fgee.co.ke/wp-content/uploads/2021/07/Dell-Optiplex-3050-ci5-4gb-500gb.jpg', category_id:desktops.id
    },
    {
-    name:'Lenovo Thinkcentre M 80s', price: 45000, description:'Intel corei5 10th generation , 8gb DDR4(upgradable) , 512 Gb Nvme SSD , comes with type c port,8  USB ports, HDMI port, display port, VGA port', image_url:'https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8NTY3Nzh8aW1hZ2UvanBnfGg1OS9oMGIvMTA5MjAxNzkyNjk2NjIuanBnfGE2OTIxMGE4OGUzNjljYmZjZDA4NTEzOThkOWQ1NzA4MTcyMzJmOTI0MTY0NmY0MDUzZDc5ZWVkYmRlNTJlMjA/bWFzdGVyfH.jpg', category_id:desktops.id
+    name:'Lenovo Thinkcentre M 80s', price: 45000, description:'Intel corei5 10th generation , 8gb DDR4(upgradable) , 512 Gb Nvme SSD ,Type c port,8  USB ports, HDMI port, display port, VGA port', image_url:'https://p1-ofp.static.pub/medias/bWFzdGVyfHJvb3R8NTY3Nzh8aW1hZ2UvanBnfGg1OS9oMGIvMTA5MjAxNzkyNjk2NjIuanBnfGE2OTIxMGE4OGUzNjljYmZjZDA4NTEzOThkOWQ1NzA4MTcyMzJmOTI0MTY0NmY0MDUzZDc5ZWVkYmRlNTJlMjA/bWFzdGVyfH.jpg', category_id:desktops.id
    },
    {
     name:'Hp Prodesk 600 G3', price:31500 , description:'Intel core i5 - 7th gen
@@ -277,12 +275,10 @@ products = [
     name:'Lenovo M710q', price: 19500, description:'Intel core i5 6th generation,
     16GB DDR4 RAM (expandable to 32GB),
     256GB NVME SSD ,
-    3.2Ghz processor speed ,
+    3.2Ghzspeed ,
     6 usb 3.o ports ,
     2 display ports ,
-    Ethernet cable ,
-    Windows 10 pro, 
-    2021 microsoft office ,', image_url:'https://p3-ofp.static.pub/fes/cms/2022/09/23/tuqhmlqr5g85dt0mxaoreax10rjtjr859744.jpg', category_id:desktops.id
+    Ethernet cable', image_url:'https://p3-ofp.static.pub/fes/cms/2022/09/23/tuqhmlqr5g85dt0mxaoreax10rjtjr859744.jpg', category_id:desktops.id
    },
   
    
