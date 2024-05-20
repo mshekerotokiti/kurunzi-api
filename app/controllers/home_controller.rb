6899a46b09dc7ddd8052d01679cf35f6a4659d66
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-20 17:26:58 +0300

    :zap: server side routing

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    def index
+      render file: 'public/index.html', layout: false
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     resources :kurunzi_shop, only: [:index, :show]
     resources :services, only: [:index, :show]
 
+    get '*path', to: 'home#index', constraints: ->(request) { !request.xhr? && request.format.html? }
+    root 'home#index'
+  end
+
   
 end
