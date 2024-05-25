8802a51d4d3f7ba3a13cbece625b11fab4bcf357
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2024-05-25 16:35:58 +0300

    added a stock column to shop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_06_181138) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_25_133427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_06_181138) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "stock"
   end
 
 end
